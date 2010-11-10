f31453de8caccefbe06c19f4406b0b0f2257b213
Ro <ro@ro-laptop.(none)>
2010-11-10 11:28:59 +0100

    modificada bbdd

--- offline/bbdd/create.sql
+++ offline/bbdd/create.sql
@@ -1,17 +1,14 @@
---drop table entradas;
-create table entradas(
+DROP TABLE IF EXISTS `entradas`;
+DROP TABLE IF EXISTS `entries`;
+CREATE TABLE entries(
   id int(10) unsigned not null auto_increment,
-  foro varchar(20) not null default 'linux', 
+  category varchar(128) not null default 'Linux', 
+  category_slug varchar(128) not null default 'Linux',
   lang varchar(20) not null default 'en',
   url varchar(255) not null,
   url_text varchar(255) not null,
-  fecha date not null,
-  creado timestamp default CURRENT_TIMESTAMP,
-  texto text,
+  url_slug varchar(255) not null,
+  original_date date not null,
+  created timestamp default CURRENT_TIMESTAMP,
+  content text,
   primary key (id));
-
-create table seguidos(
-  id int(10) unsigned not null auto_increment,
-  url varchar(255) not null,
-  creado timestamp default CURRENT_TIMESTAMP,
-  primary key (id))  
