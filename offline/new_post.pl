f440aacd97c44239a8461bf97ee777f696516456
pablo pazos <pablopr@gmail.com>
2010-11-25 10:40:47 +0100

    solo uno por idioma

--- offline/new_post.pl
+++ offline/new_post.pl
@@ -6,8 +6,7 @@ require ("dbInit.pl");
 my $posterous = Posterous->new('chnforo@imaginasoft.com','maskayerro');
 
 #send one post by category by lang
-&send_posts_by_lang($_) for qw(en pt es fr bg ca cs da fi gl el nl hu is it no pl sv tr);
-
+&send_posts_by_lang($_) for qw(en pt es fr ca da fi gl nl is it no sv);
 
 sub send_posts_by_lang{
 	my $lang = shift;
@@ -17,14 +16,16 @@ sub send_posts_by_lang{
         my $url = "";
 	my $content = "";
 	for my $category(@categories){
-		$article = &get_hash_ref_query('select * from entries_'.$lang.' where category like "'.$category.'" order by rand() limit 1');
-                $url = '<label>Link: </label>
-				<a href="http://www.knowrepository.com/'.$lang.'/'.$article->{title_slug}.'/'.$article->{id}.'" title="'.$article->{title}.'" class="left">
+		#$article = &get_hash_ref_query('select * from entries_'.$lang.' where category like "'.$category.'" order by rand() limit 1');
+#solo uno por en general, no unos por categoria por ahora.
+		$article = &get_hash_ref_query('select * from entries_'.$lang.' order by rand() limit 1');
+                $url = '<a href="http://www.knowrepository.com/'.$lang.'/'.$article->{title_slug}.'/'.$article->{id}.'" title="'.$article->{title}.'" class="left">
 '.$article->{title}.'</a>';
-		$content = $article->{summary}."<br />".$url;
+		$content = $url."<br />".$article->{summary};
                 $posterous->add_post(title => $article->{title}, body => $content);
                 print "Sending post: $article->{title} \n";
-		sleep(1);
+		sleep(5);
+                break;
 	}
 }
 
